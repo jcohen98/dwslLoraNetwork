23f7f962f0bf8504cd470937f0c5430f76fa9a9a
jcohen98 <joshuacohen98@gmail.com>
2018-01-10 14:58:00 -0500

    Cleaned up python script

--- dwsl_UDP_to_influxDB.py
+++ dwsl_UDP_to_influxDB.py
@@ -7,23 +7,42 @@ This is a simple script that is meant to recieve GNURadio UDP Messages through p
 from influxdb import InfluxDBClient
 import socket
 
-UDP_IP = "0.0.0.0"
-UDP_PORT = 0000
+#Specify UDP IP and Port to listen to
+UDP_IP = "127.0.0.1"
+UDP_PORT = 40868
+
+#Specifies the Database properties to send the information toi
+#TODO make the database and figure out these details
 HOST_DB = ''
 PORT_DB = ''
 USER_DB = ''
 PW_DB = ''
 DB_NAME = ''
 
+
 def listen_UDP(IP, PORT):
+    #initialize and bind the socket
     data = None
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((IP,PORT))
+
+    #while there is no data, keep checking for data. When we find some, exit
     while data == None:
-	    data,addr = sock.recvfrom(0000)
-	    print ("Recieved Message:", data)
+	    data,addr = sock.recvfrom(1024)
+
+    #When I run this I want to check the type of addr so I kow what I'm dealing with (removing later)
+    print(type(addr))
+    print ("Recieved Message: %s", data, "From address: %s")
+    
+    # Return the parsed version of the data
+    return parse_data(data)
+
+def parse_data(data_in)
+    #TODO write code that parses the given format into a NetID, Node ID, Data Type, and Data Value
+    return data_in
 
 def on_message(NetID, ID, TYPE, value):
+    #Convert the recieved data into a readable json format by the influxDB database
         msg_json = [
                     {
                     "measurement": "LABTEST",
@@ -37,6 +56,7 @@ def on_message(NetID, ID, TYPE, value):
                         }      
                     }
                 ]
+    #TODO send the json message to the database
 def main():
     while True:
         on_message(listen_UDP(UDP_IP,UDP_PORT))
